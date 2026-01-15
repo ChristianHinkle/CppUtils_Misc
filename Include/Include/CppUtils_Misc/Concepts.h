1fc6294f90fa7d80d890b5ee9c90f92868657eb2
b2hinkle <b2hinkle@gmail.com>
2026-01-15 18:09:43 -0500

    [cmake][cpp] Bring in utilities from `CppUtils_CustomAccessed` for wider usage.
    - Create a `TypeTraits` and `Concepts` file for our library.
        - Break up the brought in utilities into these files.
    - Utilities broght in:
        - TCallable concept
        - IsRefReturnType consteval function
        - IsConstAfterRemovingRef consteval function
    - Improve documentation for TCallable concept.

---
+++ Include/Include/CppUtils_Misc/Concepts.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
+
+#pragma once
+
+#include <CppUtils_Misc/FunctionTraits.h>
+
+
+namespace CppUtils
+{
+    /*
+    * A type which is callable (via operator()).
+    */
+    template <auto T>
+    concept TCallable = requires { typename FunctionTraits<decltype(T)>; };
+}
