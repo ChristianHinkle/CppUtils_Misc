8dc2cdc050fb394347ca6f0532256d4e62278d84
Christian Hinkle <christianhinko@gmail.com>
2026-01-30 02:05:40 -0700

    [cmake][files][cpp] Copy cmake file list improvements from CppUtils_Core: Move file lists into their own "Files.cmake" modules and include them where the cmake code uses them. So now, code writers don't have to dig through cmake code to list their source files that they add. Rename redundant "Include" and "Source" directories to "Files" as they purely hold the source/include files and correspond with the contents of the "Files.cmake".

---
+++ Source/Files.cmake
@@ -0,0 +1,18 @@
+# Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
+
+# Set this variable for the person including us to use.
+set(FILES_CMAKE_RESULT
+  # List out all our source files, relative to our "Files" directory.
+  "${MY_BASE_PROJECT_NAME_FULL}_Export.cpp"
+  "Filesystem.cpp"
+  "String.cpp"
+  "CharBufferString.cpp"
+  "DataMixin.cpp"
+  "DataChain.cpp"
+  "TypeTraits.cpp"
+  "FunctionTraits.cpp"
+  "Concepts.cpp"
+  "CommandParsing.cpp"
+  "ContainerElementType.cpp"
+  "Static_Execute.cpp"
+  )
