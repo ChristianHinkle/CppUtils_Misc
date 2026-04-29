8c2da77bbd1f134a80e32aefe394c84f5955d6d7
Christian Hinkle <christianhinko@gmail.com>
2026-04-29 08:48:06 -0600

    [cpp][format][time] Change `std::format` time ISO format string literal to use the shorthand ISO format specifiers and document them.

--- Include/Files/CppUtils/Misc/DateTime.h
+++ Include/Files/CppUtils/Misc/DateTime.h
@@ -8,8 +8,9 @@
 /**
  * @brief String literal for `std::format` to represent a time in full ISO 8601 format: yyyy-mm-ddThh:mm:ss+/-zzzz
  * @note Precision of the seconds is determined by the duration of the time point given to `std::format`. It may add a decimal point so that it can represent milliseconds, for example.
+ * @see https://cppreference.com/cpp/chrono/system_clock/formatter for format specifiers.
  */
-#define CPPUTILS_FORMAT_TIME_ISO_FULL_STRING_LITERAL "{:%Y-%m-%dT%H:%M:%S%z}"
+#define CPPUTILS_FORMAT_TIME_ISO_FULL_STRING_LITERAL "{:%FT%T%z}" // `%F` expands to `%Y-%m-%d` and `%T` expands to `%H:%M:%S`.
 
 namespace CppUtils
 {
